b65902fb57b1cb76672b7002fe0f7e6e4c445a00
Ziad-Mahmoud <37977244+Ziad-Mahmoud@users.noreply.github.com>
2019-11-11 02:33:54 +0200

    Create User model schema and User Registration using PostMan

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,48 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+
+// Load User model
+const User = require("../../models/User");
 
 // @route          Get api/users/test
 // @description    Test users route
 // @access         Puplic
 router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 
+// @route          Get api/users/register
+// @description    Register user
+// @access         Puplic
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", // Size
+        r: "pg", // Rating
+        d: "mm" // Default
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 module.exports = router;
